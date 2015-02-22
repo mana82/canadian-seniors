8cd2ce23ff092372ea1dfa3e85f3a1c160478fa3
Sampath <sampathr@ceylonit.com>
2015-02-22 14:14:00 +0530

    add: bootstrap skeleton

---
+++ skin/frontend/bootstrap/default/js/custom.js
@@ -0,0 +1,9 @@
+/*jQuery(function () {
+	jQuery('input[data-content]').popover({
+		trigger: 'focus'
+	});
+	jQuery('[data-toggle="popover"]').popover({
+		'trigger': 'hover',
+		'placement': 'right'
+	});
+});*/
